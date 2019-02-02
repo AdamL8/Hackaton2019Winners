64874f66ee60de970e6ee4d1c68294318a069155
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2019-02-02 17:42:10 -0500

    Fix display bug

--- chrome-extension/ext/src/inject/inject.js
+++ chrome-extension/ext/src/inject/inject.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+  let id = null;
+  let lang = null;
+
+  document.body.innerHTML +=
+        '<div style="position:fixed;width:50px;height:50px;opacity:0.3;z-index:100000;top:75px;right:20px;border-radius:50%;background:#000;cursor:pointer;" onClick="document.dispatchEvent(new CustomEvent(\'new_id\'));"> <img src="https://developer.android.com/images/brand/Android_Robot.png" style="width: 21px;height: 25px;margin: 50%;transform: translate(-50%, -50%);"></img></div>';
+  
+
   var config = {
     apiKey: "AIzaSyB9hrPS41Gqy9rKN_u5Yj2P7Qrh26kwRXo",
     authDomain: "cbc-hackathon-2019.firebaseapp.com",
@@ -10,36 +17,31 @@ $(document).ready(function() {
   firebase.initializeApp(config);
 
   document.addEventListener("new_id", function(e) {
-    console.log("Saving to firebase", e.detail);
-    var child = firebase.database().ref().child('queue').push();
-    child.set(e.detail);
+    if (id && lang) {
+      console.log("Saving to firebase");
+      var child = firebase.database().ref().child('queue').push();
+      child.set({id, lang});
+    }
+    alert(lang=="fr" ? "Envoyer vers votre téléphone" : "Sent to mobile");
   });
 
-  let url = window.location.href;
-  let lang = "";
-  let idRegex = null;
-  if (~url.indexOf("radio-canada.ca")) {
-    // eg: https://ici.radio-canada.ca/nouvelle/1150640/venezuela-caracas-tensions-manifestations-maduro-guaido
-    idRegex = /^.*\/([0-9]+)\/.*$/g;
-    lang = "fr";
-  } else if (~url.indexOf("cbc.ca")) {
-    // eg: https://www.cbc.ca/news/canada/montreal/mystery-solved-we-finally-know-how-the-snow-bear-got-its-belly-button-1.5002854
-    idRegex = /^.*\-\d+\.([0-9]+)$/g;
-    lang = "en";
-  }
-  if (idRegex) {
-    let id_matches = idRegex.exec(url);
-    if (id_matches && id_matches.length > 1) {
-      let id = id_matches[1];
-
-      document.body.innerHTML +=
-        '<div style="position:fixed;width:50px;height:50px;opacity:0.3;z-index:100000;top:75px;right:20px;border-radius:50%;background:#000;cursor:pointer;" onClick="document.dispatchEvent(new CustomEvent(\'new_id\', {detail: {id:\'' +
-        id +
-        '\', lang:\'' +
-        lang +
-        '\'}}));"> <img src="https://developer.android.com/images/brand/Android_Robot.png" style="width: 21px;height: 25px;margin: 50%;transform: translate(-50%, -50%);"></img></div>';
-
-      console.log("Import tensorflow as tf: matched id", id);
+  setInterval(() => {
+    let url = window.location.href;
+    let idRegex = null;
+    if (~url.indexOf("radio-canada.ca")) {
+      // eg: https://ici.radio-canada.ca/nouvelle/1150640/venezuela-caracas-tensions-manifestations-maduro-guaido
+      idRegex = /^.*\/([0-9]+)\/.*$/g;
+      lang = "fr";
+    } else if (~url.indexOf("cbc.ca")) {
+      // eg: https://www.cbc.ca/news/canada/montreal/mystery-solved-we-finally-know-how-the-snow-bear-got-its-belly-button-1.5002854
+      idRegex = /^.*\-\d+\.([0-9]+)$/g;
+      lang = "en";
     }
-  }
+    if (idRegex) {
+      let id_matches = idRegex.exec(url);
+      if (id_matches && id_matches.length > 1) {
+        id = id_matches[1];
+      }
+    }
+  }, 100); 
 });
