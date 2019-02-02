6f51fe23a05030442d7e60c032c17945ed4c06a8
Adam Letourneau <adam.letourneau1993@gmail.com>
2019-02-01 20:38:34 -0500

    WIP Signin activity necessary files

---
+++ App/android-boilerplate-master/app/src/main/java/uk/co/ribot/androidboilerplate/ui/signin/SignInPresenter.java
@@ -0,0 +1,6 @@
+package uk.co.ribot.androidboilerplate.ui.signin;
+
+import uk.co.ribot.androidboilerplate.ui.base.BasePresenter;
+
+public class SignInPresenter extends BasePresenter<SignInMvpView> {
+}
