c69c0e0557aaae4c782c5a8a2b5554550fc6f84f
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2019-02-02 12:03:06 -0500

    TTS Francais

--- middleware/server.py
+++ middleware/server.py
@@ -6,7 +6,10 @@ import threading
 import requests
 from flask import Flask, jsonify, make_response
 import schedule
+import html2text
+import re
 from tts import tts
+
 
 DEBUG_MODE = 'DEBUG' in os.environ
 
@@ -19,7 +22,7 @@ PORT = 5000 if DEBUG_MODE else 80
 CBC_BASE_URL = "https://www.cbc.ca/aggregate_api/v1/"
 RADIO_CAN_BASE_URL = "https://services.radio-canada.ca/hackathon/neuro/v1/"
 
-RADIO_CAN_API_KEY = "&client_key=31b2bb0e-85ec-4406-9b22-31c93d7e75f9"
+RADIO_CAN_API_KEY = "client_key=31b2bb0e-85ec-4406-9b22-31c93d7e75f9"
 
 CBC_ITEMS = CBC_BASE_URL + "items/?type=story&page="
 RADIO_CAN_NEWS = RADIO_CAN_BASE_URL + "most-popular-content/radcan-news?pageNumber="
@@ -72,7 +75,7 @@ def update_data():
     for i in range(1, 5):
         raw_cbc = requests.get(CBC_ITEMS + str(i))
         temp_data_cbc += list(map(mapCbcItems, raw_cbc.json()))
-        raw_radiocan = requests.get(RADIO_CAN_NEWS + str(i) + RADIO_CAN_API_KEY)
+        raw_radiocan = requests.get(RADIO_CAN_NEWS + str(i) + "&" + RADIO_CAN_API_KEY)
         radiocan_json = raw_radiocan.json()
         if "pagedList" in radiocan_json and "items" in radiocan_json["pagedList"]:
             temp_data_radiocan += list(map(mapRadioCanItems, raw_radiocan.json()["pagedList"]["items"]))
@@ -105,6 +108,19 @@ def get_cbc_content(id):
     if "body" in raw_data['typeAttributes']:
         body = raw_data['typeAttributes']['body']
         content = cbc_parse_content_recur(body)
+        content = str(content).replace(u"\u00A0", " ")
+    return {"content": content}
+
+def get_radiocan_content(id):
+    URL = RADIO_CAN_BASE_URL + "/news-stories/" + id + "?" + RADIO_CAN_API_KEY 
+    raw_data = requests.get(URL).json()
+    content = ""
+    if "body" in raw_data:
+        h = html2text.HTML2Text()
+        h.ignore_links = True
+        h.strong_mark = ""
+        h.emphasis_mark = ""
+        content = re.sub(r'<.*?>', '', raw_data["body"]["html"].replace(">", "> ")).replace("&nbsp;", " ").replace("\n", "").replace("  ", " ")
     return {"content": content}
 
 @app.route('/api/content/en', methods=['GET'])
@@ -119,6 +135,10 @@ def get_tasks_fr():
 def get_content_en(id):
     return jsonify(get_cbc_content(id))
 
+@app.route('/api/content/fr/<id>', methods=['GET'])
+def get_content_fr(id):
+    return jsonify(get_radiocan_content(id))
+
 
 @app.route('/api/tts/en/<id>')
 def get_tts_en(id):
@@ -129,7 +149,10 @@ def get_tts_en(id):
 
 @app.route('/api/tts/fr/<id>')
 def get_tts_fr(id):
-    pass
+    response = make_response(tts(get_radiocan_content(id)["content"], "fr"))
+    response.headers['Content-Type'] = 'audio/wav'
+    #response.headers['Content-Disposition'] = 'attachment; filename=sound.wav'
+    return response
 
 if __name__ == '__main__':
     update_data_thread = threading.Thread(target=data_thread)
