a4e532c65699bf157f75d6dbb784cf1d7f15d427
Adam Letourneau <adam.letourneau1993@gmail.com>
2019-02-03 05:27:00 -0500

    experience view more pimp

--- App/MyNewsCast/app/src/main/java/co/w/mynewscast/ui/experience/ExperienceActivity.java
+++ App/MyNewsCast/app/src/main/java/co/w/mynewscast/ui/experience/ExperienceActivity.java
@@ -1,11 +1,16 @@
 package co.w.mynewscast.ui.experience;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 
 import org.json.JSONArray;
@@ -41,6 +46,51 @@ public class ExperienceActivity extends BaseActivity implements ExperienceMvpVie
     private List<Article> experienceArticleList = new ArrayList<>();
 
 
+    /**
+     * RecyclerView item decoration - give equal margin around grid item
+     */
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,8 +103,13 @@ public class ExperienceActivity extends BaseActivity implements ExperienceMvpVie
         experienceArticleAdapter = new ExperienceArticleAdapter(this, experienceArticleList);
         experienceArticleRecyclerView = findViewById(R.id.experience_list_view);
 
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 1);
+        experienceArticleRecyclerView.setLayoutManager(mLayoutManager);
+
+        experienceArticleRecyclerView.addItemDecoration(new GridSpacingItemDecoration(1, dpToPx(1), true));
+        experienceArticleRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
         experienceArticleRecyclerView.setAdapter(experienceArticleAdapter);
-        experienceArticleRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         loadArticles();
 
